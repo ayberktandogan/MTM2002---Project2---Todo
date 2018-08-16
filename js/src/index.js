97919c0b471783dc51ba709581a6816e8a69f0e9
ayberktandogan <l5215066@std.yildiz.edu.tr>
2018-08-16 21:53:29 +0300

    token timeout fix

--- js/src/index.js
+++ js/src/index.js
@@ -1,81 +1,75 @@
 import axios from "../../axios";
-const Validator = require('validator')
+const Validator = require("validator");
 
 const handleLogout = () => {
-    const args = ['auth', 'token', 'userId']
-    args.map(arg => localStorage.removeItem(arg))
-    window.location = '/'
-}
+  const args = ["auth", "token", "userId"];
+  args.map(arg => localStorage.removeItem(arg));
+  window.location = "/";
+};
 
 const addTodo = () => {
-    const todo = document.getElementById('todo-input').value
+  const todo = document.getElementById("todo-input").value;
 
-    const newTodo = {
-        text: todo
-    }
+  const newTodo = {
+    text: todo
+  };
 
-    if(localStorage.getItem('auth') !== null && !Validator.isEmpty(todo)) {
-        axios.post('/todo/add/' + localStorage.getItem('userId'), newTodo)
-        .then(res => {
-            console.log(res)
-            window.location = '/'
-        })
-        .catch(err => console.log(err))
-    }
-}
+  if (localStorage.getItem("auth") !== null && !Validator.isEmpty(todo)) {
+    axios
+      .post("/todo/add/" + localStorage.getItem("userId"), newTodo)
+      .then(res => {
+        console.log(res);
+        window.location = "/";
+      })
+      .catch(err => console.log(err));
+  }
+};
 
 const deleteTodo = () => {
-    const itemId = event.target.id
-    const item = {
-        _id : itemId
-    }
-    axios.post('/todo/delete/' + localStorage.getItem('userId'), item)
+  const itemId = event.target.id;
+  const item = {
+    _id: itemId
+  };
+  axios
+    .post("/todo/delete/" + localStorage.getItem("userId"), item)
     .then(res => {
-        window.location = '/'
+      window.location = "/";
     })
-    .catch(err => console.log(err))  
-}
+    .catch(err => console.log(err));
+};
 window.onload = function onload() {
-    if(localStorage.getItem('auth') !== null) {
-        const button = "<button class='btn btn-white waves-effect' onclick='handleLogout()'>X Çıkış yap</button>"
-        document.getElementById('logout').innerHTML = button
-    }
-    if(localStorage.getItem('auth') === null) {
-        const button = "<a class=\"btn btn-white waves-effect\" href=\"./login.html\">Login</a><a class=\"btn btn-white waves-effect\" href=\"./register.html\">Register</a>"
-        document.getElementById('logout').innerHTML = button
-        document.getElementById('addButton').classList.add("disabled")
-    }
-    if(localStorage.getItem('userId') !== null || localStorage.getItem('userId') !== undefined) {
-        axios.get('/todo/' + localStorage.getItem('userId'))
-        .then(res => {
-            const listElement = document.getElementById('to-do-list')
-            const array = res.data.todos
-            array.forEach((element) => {
-                const listItem = document.createElement('li')
-                listItem.innerHTML = element.text + "<i class='fa fa-trash' id="+element._id+" onclick='deleteTodo()'></i>"
-                listElement.appendChild(listItem)
-            });
-        })
-    }
-}
-
-/*window.onload = function getList() {
-    
-
-    for (var i = 0; i < numberOfListItems; ++i) {
-        // create an item for each one
-        var listItem = document.createElement('li');
-
-        // Add the item text
-        listItem.innerHTML = listData[i];
-
-        // Add listItem to the listElement
+  if (localStorage.getItem("auth") !== null) {
+    const button =
+      "<button class='btn btn-white waves-effect' onclick='handleLogout()'>X Çıkış yap</button>";
+    document.getElementById("logout").innerHTML = button;
+  }
+  if (localStorage.getItem("auth") === null) {
+    const button =
+      '<a class="btn btn-white waves-effect" href="./login.html">Login</a><a class="btn btn-white waves-effect" href="./register.html">Register</a>';
+    document.getElementById("logout").innerHTML = button;
+    document.getElementById("addButton").classList.add("disabled");
+  }
+  if (
+    localStorage.getItem("userId") !== null ||
+    localStorage.getItem("userId") !== undefined
+  ) {
+    axios.get("/todo/" + localStorage.getItem("userId")).then(res => {
+      const listElement = document.getElementById("to-do-list");
+      const array = res.data.todos;
+      array.forEach(element => {
+        const listItem = document.createElement("li");
+        listItem.innerHTML =
+          element.text +
+          "<i class='fa fa-trash' id=" +
+          element._id +
+          " onclick='deleteTodo()'></i>";
         listElement.appendChild(listItem);
-    }
-}*/
-
+      });
+    });
+  }
+};
 
 //Exports
-window.handleLogout = handleLogout
-window.addTodo = addTodo
-window.deleteTodo = deleteTodo
+window.handleLogout = handleLogout;
+window.addTodo = addTodo;
+window.deleteTodo = deleteTodo;
--- js/src/main.js
+++ js/src/main.js
@@ -1,2 +1,13 @@
-import 'bootstrap'
-import 'mdbootstrap'
+import "bootstrap";
+import "mdbootstrap";
+import jwt_decode from "jwt-decode";
+
+if (localStorage.getItem("token")) {
+  const decoded = jwt_decode(localStorage.getItem("token"));
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    const args = ["auth", "token", "userId"];
+    args.map(arg => localStorage.removeItem(arg));
+    window.location = "/";
+  }
+}
