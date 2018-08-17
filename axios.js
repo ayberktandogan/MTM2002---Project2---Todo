3a174611d54aba2c04747ce64769a1bafc9f81b8
ayberktandogan <42430762+ayberktandogan@users.noreply.github.com>
2018-08-17 10:20:56 +0300

    Axios fix for Heroku

--- axios.js
+++ axios.js
@@ -1,7 +1,17 @@
 import axios from 'axios'
 
-const instance = axios.create({
+if (process.env.NODE_ENV !== 'production') {
+    const instance = axios.create({
     baseURL: 'http://localhost:5000/api/',
 });
+}
+
+else {
+    const instance = axios.create({
+    baseURL: '/api/',
+});
+}
+
+
 
 export default instance
