3d11e4fe4d15610a5f0ee1fe2c9c248aecf312d7
ayberktandogan <42430762+ayberktandogan@users.noreply.github.com>
2018-08-17 10:40:30 +0300

    Update axios.js

--- axios.js
+++ axios.js
@@ -1,17 +1,6 @@
 import axios from 'axios'
 
-if (process.env.NODE_ENV !== 'production') {
-    const instance = axios.create({
-    baseURL: 'http://localhost:5000/api/',
-});
-}
-
-else {
-    const instance = axios.create({
-    baseURL: 'https://todo-ytu.herokuapp.com/api/',
-});
-}
-
-
+const instance = axios.create({
+baseURL: 'https://todo-ytu.herokuapp.com/api/',
 
 export default instance
